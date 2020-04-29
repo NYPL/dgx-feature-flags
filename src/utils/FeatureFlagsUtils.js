637b3b80657c8c34cbf90c441e0a7ee1b660b05f
Gregory Kallenberg <gregorykallenberg@nypl.org>
2020-04-29 11:23:51 -0400

    replace tabs with spaces

--- src/utils/FeatureFlagsUtils.js
+++ src/utils/FeatureFlagsUtils.js
@@ -1,10 +1,10 @@
 import FeatureFlagsActions from '../actions/FeatureFlagsActions.js';
 
 export default {
-	activateFeature(feature) {
-		return FeatureFlagsActions.activateFeature(feature);
-	},
-	deactivateFeature(feature) {
-		return FeatureFlagsActions.deactivateFeature(feature);
-	},
+  activateFeature(feature) {
+    return FeatureFlagsActions.activateFeature(feature);
+  },
+  deactivateFeature(feature) {
+    return FeatureFlagsActions.deactivateFeature(feature);
+  },
 };
